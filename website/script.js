b65f8a4fa867fb4644843f16306654a1ff59497e
Krishan T <krishan0818@gmail.com>
2021-08-28 13:16:44 -0400

    trying to make the face api actually play the alert

--- website/script.js
+++ website/script.js
@@ -1,9 +1,9 @@
 const video = document.getElementById('video')
 
 Promise.all([
-    faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
-    faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
-    faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+    faceapi.nets.tinyFaceDetector.loadFromUri("./models"),
+    faceapi.nets.faceRecognitionNet.loadFromUri("./models"),
+    faceapi.nets.faceLandmark68Net.loadFromUri('./models'),
 ]).then(startVideo)
 
 function startVideo() {
@@ -17,8 +17,26 @@ function startVideo() {
 
 
 video.addEventListener('play', () => {
+    const canvas = faceapi.createCanvasFromMedia(video)
+    document.body.append(canvas)
+    const displaySize = { width: video.width, height: video.height }
+    faceapi.matchDimensions(canvas, displaySize)
     setInterval(async () => {
         const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks()
-        console.log(detections)
+        const resizedDetections = faceapi.resizeResults(detections, displaySize)
+        canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+        faceapi.draw.drawDetections(canvas, resizedDetections)
+        faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
     }, 100);
 })
+
+
+var alertsound = new Audio('alertjingle.mp3');
+var playSound = function() {
+    if (faceapi.scoreThreshold < 0.5 ) {
+    alertsound.play();
+    console.log("Alert Sound Played")
+    }
+}
+
+playSound();
